7317170ee280d6ada15abd3e8e8353fe6987bca5
Matthias Müringer <mmueringer@web.de>
2022-12-06 15:04:40 +0100

    Created express code for routes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express'),
 bodyParser = require('body-parser'),
 uuid = require('uuid'),
@@ -54,17 +55,76 @@ let myMovies = [
     },
   ];
 
-// GET all movies
+// Gets the list of data about ALL movies
 app.get('/movies', (req, res) => {
     res.json(myMovies);
 });
 
+// Gets data about a single movie by title
+app.get('/movies/:title', (req, res) => {
+    res.json(myMovies.find((movie) => {
+        return movie.title === req.params.title}));
+});
+
+// Gets data about a genre by title
+app.get('/movies/genres/:title', (req, res) => {
+    res.json(myMovies.find((movie) => {
+        return movie.title === req.params.title}));
+});
+
+
+// Get data about a director
+app.get('/directors/:name', (req, res) => {
+    res.json(myMovies.find((movie) => {
+        return movie.director.name === req.params.name;
+    }));
+});
+
+// Register new user
+app.post('/users', (req, res) => {
+  
+});
+
+// Update user information
+app.put('/users/:user', (req, res) => {
+    
+});
+
+// Add movie to user favorites
+app.post('/users/:favorites/movies/:movie/', (req, res) => {
+    let newMovie = req.body;
+
+    if (!newMovie.title) {
+        const message = 'Missing title in request body';
+        res.status(404).send(message);
+    } else {
+        newMovie.id = uuid.v4();
+        myMovies.push(newMovie);
+        res.status(201).send(newMovie);
+    }
+});
+
+// Remove movie from user favorites
+app.delete('/users/movies/:movie/title', (req, res) => {
+
+});
+
+// Deregister exisiting user
+app.post('users/:deregister', (req, res) => {
+
+});
+
+// Serve static files
 app.use('/public', express.static(__dirname + '/public'));
 
+
+// Handling errors in middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+
+
 
 // Listen for requests
 app.listen(8080, () => {
