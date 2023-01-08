91bf1420dbaa6dbbbebf767d4dc85a8e81c05b58
Matthias Müringer <mmueringer@web.de>
2023-01-08 15:12:43 +0100

    Updated allowedOrigins

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080','http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080','http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {
